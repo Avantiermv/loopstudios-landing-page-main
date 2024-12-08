0f0fc01a4e221a0645c970e3a6f41be9cb47a314
Marcos Vinicius <avantiermv@gmail.com>
2024-12-08 19:51:14 -0300

    Menu mobile and function.
    Today I addicted more information on the footer, changed a few things in the creations section and addicted a function to the menu mobile.

---
+++ logic.js
@@ -0,0 +1,10 @@
+const menuMobile = document.getElementById('menumobile')
+
+function menu(){
+   menuMobile.style.display = "block";
+   menuMobile.style.transition= "1.7s";
+}
+
+function left(){
+    menuMobile.style.display = "none";
+   }
